91267236ce75490529b200ebf78468cbbd9ede35
Cristian Lemes <cristianlemes343@gmail.com>
2026-01-16 10:22:25 -0300

    Codigo base comcluido

--- obj/Release/net8.0/ProjetoTCN.AssemblyInfo.cs
+++ obj/Release/net8.0/ProjetoTCN.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoTCN")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd47775eef92c5e5ef8dca15ec89090cf103a1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoTCN")]
